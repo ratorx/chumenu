0fb176e151231099c981aafaa590e388c0a548cd
Reetobrata Chatterjee <reetobratachatterjee@gmail.com>
2017-04-16 01:11:50 +0100

    Test (#1)
    * Move API functions to own file
    
    * Move testing configuration to own branch
    
    * Remove test specific changes from code

--- node/app.js
+++ node/app.js
@@ -1,4 +1,3 @@
-
 const
   bodyParser = require('body-parser'),
   config = require('config'),
@@ -7,17 +6,13 @@ const
   https = require('https');
   request = require('request');
   path = require('path');
+  api = require('./facebook');
 
-process.title = "chumenu"
-const TEST_MODE = (process.argv[2] == "test");
-if (TEST_MODE) console.log("Using testing configuration");
+process.title = "chumenu";
 
 // These values should be set in config/default.json
 const
-  APP_SECRET = config.get('appSecret'),
   VALIDATION_TOKEN = config.get('validationToken'),
-  PAGE_ACCESS_TOKEN = config.get('pageAccessToken'),
-  SERVER_URL = config.get('serverURL');
   PORT = config.get('port')
   KEY = fs.readFileSync(config.get("key")),
   CERT = fs.readFileSync(config.get("cert")),
@@ -27,10 +22,10 @@ const
     cert: CERT,
     ca: CA
   },
-  USER_FILE = TEST_MODE ? config.get("testusers"): config.get("users"),
+  USER_FILE = config.get("users"),
   MENU_FILE = config.get("menu");
 
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL && PORT && KEY && CERT && CA && USER_FILE)) {
+if (!(VALIDATION_TOKEN && PORT && KEY && CERT && CA && USER_FILE)) {
   console.error("Set the appropriate config values in config/default.json");
   process.exit(1);
 };
@@ -47,6 +42,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("public"))
 
+// Server initialisation
+const server = https.createServer(CREDS, app).listen(PORT, function(err) {
+  if (err) {
+    return console.log("Something went wrong.");
+  }
+
+  console.log(`Listening on port ${PORT}`);
+});
+
 // Webhook Verification
 app.get("/webhook", function(req, res) {
   // console.log("Incoming request");
@@ -62,6 +66,7 @@ app.get("/webhook", function(req, res) {
 
 // Callback from Facebook
 app.post('/webhook', function (req, res) {
+// From https://github.com/fbsamples/messenger-platform-samples
   var data = req.body;
 
   if (data.object === "page") {
@@ -80,19 +85,16 @@ app.post('/webhook', function (req, res) {
   res.sendStatus(200);
 });
 
+// Hook for sending messages to all subscribers
 app.post('/sendall', function(req, res) {
   var data = req.body;
   var message = data.meal + ":";
   if (data.validationToken === VALIDATION_TOKEN) {
     // Deal with implicit array to string of size 1 array
-    if (typeof data.menu === "string") {
-      message += "\n -  " + data.menu;
-    } else {
-      data.menu.forEach( (item) => message += "\n -  " + item);
-    }
+    constructMessage(data.menu);
     // Message subscribers
     for(var id in users) {
-      textMessage(id, message);
+      api.textMessage(id, message);
     }
     res.sendStatus(200);
   } else {
@@ -101,7 +103,9 @@ app.post('/sendall', function(req, res) {
   }
 })
 
+// Hook for updating stored menu
 app.post('/updatemenu', function(req, res) {
+  // console.log("Updating menu");
   var data = req.body;
   if (data.validationToken === VALIDATION_TOKEN) {
     (function(callback) {
@@ -119,6 +123,7 @@ app.post('/updatemenu', function(req, res) {
   }
 })
 
+// Parse received message - TODO: Make it more modular to match cases
 function receivedMessage(event) {
   // Handle different cases - currently only text messages supported.
   switch (event.message.text) {
@@ -131,10 +136,10 @@ function receivedMessage(event) {
           callback();
         })(function() {
           fs.writeFileSync(USER_FILE, JSON.stringify(users));
-          textMessage(event.sender.id, "You have been subscribed to receive menu messages.");
+          api.textMessage(event.sender.id, "You have been subscribed to receive menu messages.");
         });
       } else {
-        textMessage(event.sender.id, "You are already subscribed.");
+        api.textMessage(event.sender.id, "You are already subscribed.");
       }
       break;
     case "unsubscribe":
@@ -145,10 +150,10 @@ function receivedMessage(event) {
           callback();
         })(function() {
           fs.writeFileSync(USER_FILE, JSON.stringify(users));
-          textMessage(event.sender.id, "You have been unsubscribed from receiving menu messages.");
+          api.textMessage(event.sender.id, "You have been unsubscribed from receiving menu messages.");
         });
       } else {
-        textMessage(event.sender.id, "You are not currently subscribed to receive menu messages.");
+        api.textMessage(event.sender.id, "You are not currently subscribed to receive menu messages.");
       }
       break;
     case "lunch":
@@ -163,61 +168,25 @@ function receivedMessage(event) {
       break;
     case "help":
     case "Help":
-      textMessage(event.sender.id, "Type subscribe to get menu alerts.\nType unsubscribe to stop getting menu alerts.\nType lunch to get today's lunch.\nType dinner to get today's dinner.");
+      api.textMessage(event.sender.id, "Type subscribe to get menu alerts.\nType unsubscribe to stop getting menu alerts.\nType lunch to get the next lunch menu.\nType dinner to get the next dinner menu.");
     default:
-      // textMessage(event.sender.id, "Unrecognised. Supported operations are subscribe and unsubscribe.");
+      // api.textMessage(event.sender.id, "Unrecognised. Supported operations are subscribe and unsubscribe.");
       break;
   }
 }
 
-function textMessage(recipientId, messageText) {
-  // A wrapper function for sending text messages - included for future expansion
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText,
-      metadata: "Hello"
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  // Generic send function courtesy of https://github.com/fbsamples/messenger-platform-samples
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (error || response.statusCode != 200) {
-      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-    }
-  });
-}
-
+// Send menu message on request
 function menuMessage(mealname, id) {
   var message = mealname + ":";
   var meal = menu[mealname];
-  if (!meal) {
-    message = "No data available."
-  } else if (typeof meal === "string") {
-    message += "\n -  " + meal;
-  } else {
-    meal.forEach( (item) => message += "\n -  " + item);
-  }
-  textMessage(id, message);
+  message += constructMessage(meal)
+  api.textMessage(id, message);
 }
 
-
-const server = https.createServer(CREDS, app).listen(PORT, function(err) {
-  if (err) {
-    return console.log("Something went wrong.");
-  }
-
-  console.log(`Listening on port ${PORT}`);
-});
+function constructMessage(menuinfo) {
+  var message = ""
+  if (!menuinfo) message = "\n - TBC";
+  else if (typeof menuinfo === "string") message += "\n -  " + menuinfo;
+  else menuinfo.forEach( (item) => message += "\n -  " + item);
+  return message;
+}
